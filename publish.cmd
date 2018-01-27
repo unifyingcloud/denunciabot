62bd9fe1d5548d1600de3ff27e4cd4c3d11ab4a8
Jonatan <jonatane.cisneros@gmail.com>
2018-01-27 20:38:37 +0100

    update bot code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=descuentos-ninja-Web-Deploy.pubxml -p:Password=T51Mbi4BD1pZkXLKBwJlpgReil53JEtHEi72oWAd2shs2Th9qwdBWFhsgFcv
+
